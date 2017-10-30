65d24d644eb1cfa5d8f06a91376b57a72c97c771
jordangonen <jordangonen@wustl.edu>
2017-10-29 22:32:44 -0500

    feeling dumb
    still tryna do this css thing

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8000);
+});
+
+    extensions = {
+      ".html" : "text/html",
+      ".css" : "text/css",
+      ".js" : "application/javascript",
+      ".png" : "image/png",
+      ".gif" : "image/gif",
+      ".jpg" : "image/jpeg"
+    };

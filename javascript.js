65c45815b5fae9576e74ca230fd087324e93f0aa
jordangonen <jordangonen@wustl.edu>
2017-11-14 18:23:19 -0600

    eh

--- javascript.js
+++ javascript.js
@@ -46,6 +46,92 @@ particle.getVariable({ deviceId: deviceId, name: "varState", auth: token }).then
   // console.log('An error occurred while getting attrs:', err);
 });
 
+
+//assignment 5
+
+var particle = new Particle();
+var productId = "6153";
+var clientId = "washu-garage-door-5162";
+var clientToken = "882ec3178524a02ad8bdb1473fd6aaa143f5a38c";
+
+var deviceOneId = "2e0060000e51353338363333";
+// var deviceTwoId = "2e0060000e51353338363333"
+
+
+document.getElementById("login-btn").addEventListener("click",function() {
+  var username = document.getElementById('email').value;
+  var pass = document.getElementById('pw').value;
+
+  particle.login({username: username, password: pass}).then(logSuccess, logFail);
+
+function logSuccess(data) {
+  token = data.body.access_token;
+  particle.getEventStream({ deviceId: deviceOneId, auth: token }).then(function(stream) {
+  stream.on('event', stateMover)
+  });
+
+}
+function logFail(data){
+  console.log('Could not log in.', err);
+}
+
+// new registartion
+
+document.getElementById("create-btn").addEventListener("click",function() {
+  var email = document.getElementById('email-registration').value;
+  var password = document.getElementById('pw-registration').value;
+
+  particle.createCustomer( {productId:productId, clientId:clientId, clientToken:clientToken, customerEmail:email, customerPassword:password} )
+   .then(saveTokenAndClaimDeviceOne)
+   // .then(claimDeviceTwo)
+   .then(doneClaimingDevices)
+   .catch(errorClaimingDevices)
+
+   function saveTokenAndClaimDeviceOne(data) {
+    console.log("Success creating customer; Claiming Device One");
+    console.dir(data)
+    customerToken = data.body.access_token;
+    console.log(customerToken)
+    // Return a "promise" object (so .then() can be used)
+
+    return particle.claimDevice({deviceId:deviceOneId, requestTransfer:true, auth:customerToken})
+   }
+
+});
+var customerToken = "LEAVE ALONE";
+
+particle.createCustomer = function ({ productId, clientId, clientToken, customerEmail, customerPassword }) {
+ const auth = clientId + ':' + clientToken;
+ const uri = `/v1/products/${productId}/customers`;
+ return this.post(uri, {productIdOrSlug:productId, client_id:clientId, client_secret:clientToken, email:customerEmail, password:customerPassword
+ }, auth, this.context);
+}
+
+// function claimDeviceTwo(data) {
+//  console.log("Success claiming device one; Claiming Device Two");
+//  console.dir(data)
+//  // Return a "promise" object (so .then() can be used)
+//  return particle.claimDevice({deviceId:deviceTwoId, requestTransfer:true, auth:customerToken})
+// }
+
+function doneClaimingDevices() {
+ console.log("Done Claiming Devices");
+}
+
+function errorClaimingDevices() {
+ alert("Wrong password/username");
+ console.log("Error Claiming Devices");
+}
+
+
+
+
+
+
+
+
+
+
 var name;
 var currentStateDoor;
 // statemover function that is called when eventStream is received
@@ -452,7 +538,6 @@ document.getElementById("login-btn").addEventListener("click", function() {
 
         var email = document.getElementById('email').value;
         var password = document.getElementById('pw').value;
-
 
 
         if (email == user1.email && password == user1.password ) {
